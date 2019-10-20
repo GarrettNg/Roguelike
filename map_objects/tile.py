a61cbaad3d35bf2d0a65bbb916ee95f5a619fa07
Garrett Ng <56460265+GarrettNg@users.noreply.github.com>
2019-10-20 19:33:46 -0400

    Add Entity, Render Functions, Map

---
+++ map_objects/tile.py
@@ -0,0 +1,12 @@
+class Tile:
+    '''
+    A tile on a map. It can be blocked or can block line of sight.
+    '''
+    def __init__(self, blocked, block_sight=None):
+        self.blocked = blocked
+
+        # If a tile is blocked, it also blocks sight
+        if block_sight is None:
+            block_site = blocked
+        
+        self.block_sight = block_site
